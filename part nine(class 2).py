16215f44dfd9bf119303b45ea87fb939b3fb2362
Soheil Sarami <139607653+soheilsa99@users.noreply.github.com>
2023-11-04 15:39:43 +0330

    part nine(class 2).py

--- part nine(class 2).py
+++ part nine(class 2).py
@@ -1,201 +1,201 @@
-# print("working with a class and instances")
-# print("Example : The Car Class")
-# class Car:
-#     """A simple attempt to represent a car."""
-#     def __init__(self, make, model, year):
-#         """Initialize attributes to describe a car."""
-#         self.make = make
-#         self.model = model
-#         self.year = year
-#
-#
-#     def get_descriptive_name(self):
-#         """Return a neatly formatted descriptive name."""
-#         long_name = f"{self.year} {self.make} {self.model} "
-#         return long_name.title()
-#
-#
-# my_new_car = Car('audi', 'a4', 2024)
-# print(my_new_car.get_descriptive_name())
+print("working with a class and instances")
+print("Example : The Car Class")
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model} "
+        return long_name.title()
+
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
 
 #----------------------------------------------------------------------------------------------------------------
-# print("Setting a Default Value for an Attribute")
+print("Setting a Default Value for an Attribute")
 
-# class Car:
-#     """A simple attempt to represent a car."""
-#     def __init__(self, make, model, year):
-#         """Initialize attributes to describe a car."""
-#         self.make = make
-#         self.model = model
-#         self.year = year
-#         self.odometer_reading = 0
-#
-#     def get_descriptive_name(self):
-#         """Return a neatly formatted descriptive name."""
-#         long_name = f"{self.year} {self.make} {self.model} "
-#         return long_name.title()
-#
-#     def read_odometer(self):
-#         """Print statement showing the car's mileage."""
-#         print(f"This car has {self.odometer_reading} miles on it.")
-#
-#
-# my_new_car = Car('audi', 'a4', 2024)
-# print(my_new_car.get_descriptive_name())
-# my_new_car.read_odometer()
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model} "
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
 
 #----------------------------------------------------------------------------------------------------------------
-# print("Modifying Attribute Values")
+print("Modifying Attribute Values")
 # this ex : modifying attribute's value DIRECTLY
 
 
-# class Car:
-#     """A simple attempt to represent a car."""
-#     def __init__(self, make, model, year):
-#         """Initialize attributes to describe a car."""
-#         self.make = make
-#         self.model = model
-#         self.year = year
-#         self.odometer_reading = 0
-#
-#     def get_descriptive_name(self):
-#         """Return a neatly formatted descriptive name."""
-#         long_name = f"{self.year} {self.make} {self.model} "
-#         return long_name.title()
-#
-#     def read_odometer(self):
-#         """Print statement showing the car's mileage."""
-#         print(f"This car has {self.odometer_reading} miles on it.")
-#
-#
-# my_new_car = Car('audi', 'a4', 2024)
-# print(my_new_car.get_descriptive_name())
-# my_new_car.odometer_reading = 23
-# my_new_car.read_odometer()
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-# print("Modifying an Attribute's Value Through a Method")
-# class Car:
-#     """A simple attempt to represent a car."""
-#     def __init__(self, make, model, year):
-#         """Initialize attributes to describe a car."""
-#         self.make = make
-#         self.model = model
-#         self.year = year
-#         self.odometer_reading = 0
-#
-#     def get_descriptive_name(self):
-#         """Return a neatly formatted descriptive name."""
-#         long_name = f"{self.year} {self.make} {self.model} "
-#         return long_name.title()
-#
-#     def read_odometer(self):
-#         """Print statement showing the car's mileage."""
-#         print(f"This car has {self.odometer_reading} miles on it.")
-#
-#     def update_odometer(self, mileage):
-#         """
-#         Set the odometer reading to the given value.
-#         Reject the change if it attempts to roll the odometer back.
-#         """
-#         if mileage >= self.odometer_reading:
-#             self.odometer_reading = mileage
-#         else:
-#             print("You can't roll back an odometer!")
-#
-#
-# my_new_car = Car('audi', 'a4', 2024)
-# print(my_new_car.get_descriptive_name())
-# my_new_car.odometer_reading = 234
-# my_new_car.read_odometer()
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model} "
+        return long_name.title()
 
-# print("Incrementing an Attribute's Value Through a Method")
-# class Car:
-#     """A simple attempt to represent a car."""
-#     def __init__(self, make, model, year):
-#         """Initialize attributes to describe a car."""
-#         self.make = make
-#         self.model = model
-#         self.year = year
-#         self.odometer_reading = 0
-#
-#     def get_descriptive_name(self):
-#         """Return a neatly formatted descriptive name."""
-#         long_name = f"{self.year} {self.make} {self.model} "
-#         return long_name.title()
-#
-#     def read_odometer(self):
-#         """Print statement showing the car's mileage."""
-#         print(f"This car has {self.odometer_reading} miles on it.")
-#
-#     def update_odometer(self, mileage):
-#         """
-#         Set the odometer reading to the given value.
-#         Reject the change if it attempts to roll the odometer back.
-#         """
-#         if mileage >= self.odometer_reading:
-#             self.odometer_reading = mileage
-#         else:
-#             print("You can't roll back an odometer!")
-#
-#     def increment_odometer(self, miles):
-#         """Add the given amount to the odometer reading."""
-#         self.odometer_reading += miles
-#
-#
-# my_used_car = Car('subaru', 'outback', 2019)
-# print(my_used_car.get_descriptive_name())
-#
-# my_used_car.update_odometer(23_500)
-# my_used_car.read_odometer()
-#
-# my_used_car.increment_odometer(100)
-# my_used_car.read_odometer()
-#
-# my_used_car.increment_odometer(-500)
-# my_used_car.read_odometer()
+    def read_odometer(self):
+        """Print statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
 
-# print("-------------------------------------------------------------------------------")
-# print("Practice four : Number Served ")
-#
-# class Restaurant:
-#     """A simpl restaurant class with 3 method for show restaurant name open and cuisine type"""
-#
-#     def __init__(self, restaurant_name, cuisine_type):
-#         """Initialize cuisine type and restaurant name."""
-#         self.restaurant_name = restaurant_name
-#         self.cuisine_type = cuisine_type
-#         self.number_served = 0
-#
-#     def describe_restaurant(self):
-#         """Describe a restaurant ."""
-#         print(f"The restaurant is called {self.restaurant_name}")
-#         print(f"The cuisine type is: {self.cuisine_type}")
-#
-#     def open_restaurant(self):
-#         """Describe that restaurant is open."""
-#         print(f"The restaurant {self.restaurant_name} is open.")
-#
-#     def set_number_served(self, guests):
-#         """Set the number of served"""
-#         self.number_served = guests
-#
-#     def increment_number_served(self, increment):
-#         """Add a given number to the number of served guests"""
-#         self.number_served += increment
-#
-#
-# restaurant = Restaurant('Sophia', 'French')
-# restaurant.describe_restaurant()
-# print(f"Number of served: {restaurant.number_served}")
-#
-# restaurant.number_served = 200
-# print(f"Number served: {restaurant.number_served}")
-#
-# restaurant.set_number_served(300)
-# print(f"Number served: {restaurant.number_served}")
-#
-# restaurant.increment_number_served(700)
-# print(f"Number served: {restaurant.number_served}")
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+print("Modifying an Attribute's Value Through a Method")
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model} "
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 234
+my_new_car.read_odometer()
+
+print("Incrementing an Attribute's Value Through a Method")
+class Car:
+    """A simple attempt to represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model} "
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+
+my_used_car = Car('subaru', 'outback', 2019)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(-500)
+my_used_car.read_odometer()
+
+print("-------------------------------------------------------------------------------")
+print("Practice four : Number Served ")
+
+class Restaurant:
+    """A simpl restaurant class with 3 method for show restaurant name open and cuisine type"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initialize cuisine type and restaurant name."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        """Describe a restaurant ."""
+        print(f"The restaurant is called {self.restaurant_name}")
+        print(f"The cuisine type is: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        """Describe that restaurant is open."""
+        print(f"The restaurant {self.restaurant_name} is open.")
+
+    def set_number_served(self, guests):
+        """Set the number of served"""
+        self.number_served = guests
+
+    def increment_number_served(self, increment):
+        """Add a given number to the number of served guests"""
+        self.number_served += increment
+
+
+restaurant = Restaurant('Sophia', 'French')
+restaurant.describe_restaurant()
+print(f"Number of served: {restaurant.number_served}")
+
+restaurant.number_served = 200
+print(f"Number served: {restaurant.number_served}")
+
+restaurant.set_number_served(300)
+print(f"Number served: {restaurant.number_served}")
+
+restaurant.increment_number_served(700)
+print(f"Number served: {restaurant.number_served}")
 
 
 print("-------------------------------------------------------------------------------")
