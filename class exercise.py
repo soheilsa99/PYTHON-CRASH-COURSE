2b8cddf0e584faf5fbc49f4973f571912d6479ab
Soheil Sarami <139607653+soheilsa99@users.noreply.github.com>
2023-11-13 22:52:35 +0330

    Add files via upload

---
+++ class exercise.py
@@ -0,0 +1,16 @@
+print("-------------------------------------------------------------------------------")
+print("Practice ten : Imported_restaurant")
+from restaurant import Restaurant
+restaurant1 = Restaurant('Sophia', 'French')
+restaurant2 = Restaurant('Mia', 'Spain')
+
+restaurant1.describe_restaurant()
+restaurant2.describe_restaurant()
+
+print("-------------------------------------------------------------------------------")
+print("Practice eleven : Imported_Admin")
+from Admin_11 import Admin
+
+admin = Admin('Soheil', 'Admin', 'nbc', 21, ['can add post', 'can delete post', 'can be user'])
+admin.privileges.show_privileges()
+
